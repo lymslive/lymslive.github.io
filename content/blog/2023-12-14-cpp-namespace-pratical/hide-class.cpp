3df1a82341a6f7855d9f8b0493f0b297cfac5f03
lymslive <403708621@qq.com>
2023-12-19 18:02:00 +0800

    writting draf

---
+++ content/blog/2023-12-14-cpp-namespace-pratical/hide-class.cpp
@@ -0,0 +1,40 @@
+namespace mymod
+{
+
+struct CMyClass
+{
+    CMyClass(/* init-args */);
+    void Foo() { /* todo */ }
+    void Bar() { /* todo */ }
+
+    // data member ...
+};
+
+CMyClass* GetInstance()
+{
+    static CMyClass instance(/* defalut-args */);
+    return &instance;
+}
+
+CMyClass* NewObject(/* init-args */)
+{
+    CMyClass* pObject = new CMyclass(/* init-args */);
+    return pObject;
+}
+
+void FreeObject(CMyClass* pObject)
+{
+    delete pObject;
+}
+
+void Foo(CMyClass* pObject)
+{
+    pObject->Foo();
+}
+
+void Bar(CMyClass* pObject)
+{
+    pObject->Bar();
+}
+
+}
---
+++ content/blog/2023-12-14-cpp-namespace-pratical/hide-class.h
@@ -0,0 +1,12 @@
+namespace mymod
+{
+
+struct CMyClass;
+CMyClass* GetInstance();
+CMyClass* NewObject(/* init-args */);
+void FreeObject(CMyClass* pObject);
+void Foo(CMyClass* pObject);
+void Bar(CMyClass* pObject);
+
+}
+
