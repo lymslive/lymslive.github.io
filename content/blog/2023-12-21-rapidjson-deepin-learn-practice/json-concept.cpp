f14517fac7bf903aae75f0f0e7c8848a7f4b9b02
lymslive <403708621@qq.com>
2023-12-22 17:43:35 +0800

    writting draft

---
+++ content/blog/2023-12-21-rapidjson-deepin-learn-practice/json-concept.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <stdio.h>
+
+using std::string;
+using std::vector;
+using std::map;
+
+struct json_t {
+    union JsonValue {
+        union Scalar {
+            union Number {
+                int i;
+                uint32_t u;
+                int64_t l;
+                uint64_t ul;
+                double d;
+                float f;
+            } num_;
+            bool b;
+            string* str;
+        } scaler_;
+        union Polymer {
+            vector<json_t>* array;
+            map<string, json_t>* object;
+        } polymer_;
+        void* null_;
+    } value_;
+
+    enum JsonType {
+        JSON_NUMBER, JSON_STRING,
+        JSON_ARRAY, JSON_OBJECT,
+        JSON_BOOL, JSON_NULL,
+    } type_;
+};
+
+int main()
+{
+    printf("sizeof jsont_t = %ld\n", sizeof(json_t));
+}
